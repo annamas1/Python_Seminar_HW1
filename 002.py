7df75ec10128ca6165255410129dc241b5443081
anna maslova <zorina.a.1@yandex.ru>
2022-11-17 16:07:19 +0300

    002

---
+++ 002.py
@@ -0,0 +1,15 @@
+#2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+
+list_number = []
+for i in range(5):
+    a=int(input())
+    list_number.append(a)
+print(list_number)
+max=list_number[0]
+for i in range(len(list_number)):
+   if (list_number[i]>max):
+    max=list_number[i]
+print(max)
+
+# a=[1, 4, 8, 7, 5]
+# print(max(1, 4, 998, 7, 5))
