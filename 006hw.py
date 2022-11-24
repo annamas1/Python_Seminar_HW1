e2406e22ede8dc0467ef4d2fa24a4606e391fb50
anna maslova <zorina.a.1@yandex.ru>
2022-11-24 17:13:37 +0300

    016 домашка

--- 006hw.py
+++ 006hw.py
@@ -21,3 +21,10 @@ else:
     print(f"Утверждение ложно")
 
 
+
+# for x in range(2):
+#         for y in range(2):
+#             for z in range(2):
+#                 print(not (x or y or z) == (not x and not y and not z))
+
+
