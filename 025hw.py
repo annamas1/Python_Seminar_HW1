1e83d8b4aa39d5e609d484c1b7004b67d16ae30a
anna maslova <zorina.a.1@yandex.ru>
2022-12-05 17:00:51 +0300

    025

--- 025hw.py
+++ 025hw.py
@@ -13,13 +13,14 @@ min_list = min(list)
 N = max_list - min_list
 print(f'{list} Pазница между максимальным и минимальным значением: {round(N, 2)}')
 
+
 # рабочее решение:
 # lst = list(map(float, input("Введите числа через пробел:\n").split()))
 # new_lst = [round(i%1,2) for i in lst if i%1 != 0]
 # print(max(new_lst) - min(new_lst))
 
 
-# Решение преподователя (в мах 1 а в мин 0 потому что)
+# Решение преподователя (в мах 1 а в мин 0 потому что..)
 # list = [1.1, 1.2, 3.1, 5, 10.01]
 # min = 1
 # max = 0
