b4cc4d07c6cc18d15343f430e6635c82bf92b58c
anna maslova <zorina.a.1@yandex.ru>
2022-12-07 16:32:22 +0300

    034 HW

--- 034hw.py
+++ 034hw.py
@@ -15,3 +15,17 @@ exit()
 from math import pi
 d =  int(input("Введите число: \n"))
 print(f'число Пи с заданной точностью {d} равно {round(pi, d)}')
+
+
+
+import math
+d = input('Введите число d указывающее степень округления числа pi ')
+d = d[2:len(d)]
+print(round(math.pi,len(d)))
+
+
+a = int(input('введите нужную точность 1#= '))
+pi_target = 0
+for i in range(1, 1000000):
+pi_target += 4 * ((-1) ** (i + 1)) / (2 * i - 1)
+print(str(pi_target)[:a + 2])
