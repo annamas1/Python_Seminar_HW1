98aff2d098774f15897f281e3fe880d851d272ce
anna maslova <zorina.a.1@yandex.ru>
2022-12-07 16:33:03 +0300

    034 HW

--- 034hw.py
+++ 034hw.py
@@ -27,5 +27,5 @@ print(round(math.pi,len(d)))
 a = int(input('введите нужную точность 1#= '))
 pi_target = 0
 for i in range(1, 1000000):
-pi_target += 4 * ((-1) ** (i + 1)) / (2 * i - 1)
+    pi_target += 4 * ((-1) ** (i + 1)) / (2 * i - 1)
 print(str(pi_target)[:a + 2])
