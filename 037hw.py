7a24b36c5940b15297c4ef764125f849ae8d9859
anna maslova <zorina.a.1@yandex.ru>
2022-12-08 19:52:00 +0300

    037 HW и много вяких решалок

--- 037hw.py
+++ 037hw.py
@@ -102,3 +102,34 @@ data.close()
 # fout = open('output.txt', 'w')
 # fout.write(''.join(ans))
 # fout.close()
+
+
+
+
+# еще решение без файла
+# from random import randint
+# max_val=100
+# k = int(input('Введите натуральную степень k:'))
+# # коэфф. при старшей степени не должен быть равен 0
+# koeff=[randint(0,max_val) for i in range(k)]+[randint(1,max_val)]
+# poly='+'.join([f'{(j,"")[j==1]}x^{i}' for i, j in enumerate(koeff) if j][::-1])
+# # Поиск и замены:
+# poly=poly.replace('x^1+', 'x+')
+# poly=poly.replace('x^0', '')
+# poly+=('','1')[poly[-1]=='+']
+# poly=(poly, poly[:-2])[poly[-2:]=='^1']
+# print(poly)
+
+
+
+# еще решение без файла
+# from random import randint
+# from sympy import symbols
+# from math import prod
+ 
+# max_val=100
+# k = int(input('Введите натуральную степень k:'))
+# # коэфф. при старшей степени не должен быть равен 0
+# koeff=[randint(-max_val ,max_val) for i in range(k)]+[randint(1,max_val)]
+# x = symbols('x')
+# print (sum(map(prod,zip(koeff,[x**i for i in range(k+1)]))), '= 0')
