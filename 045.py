3353bcc35168d29a89575c58a239671095de1b1b
anna maslova <zorina.a.1@yandex.ru>
2022-12-09 14:24:53 +0300

    done

--- 045.py
+++ 045.py
@@ -4,3 +4,28 @@ a = int(input('Первое число '))
 b = int(input('Второе число '))
 import math
 print(" НОД двух чисел ", math.gcd(a, b))
+
+
+
+# =========
+a=20
+b=75
+while a != b:
+    if a > b:
+        a -= b
+    else:
+        b -= a
+
+print(a)
+
+
+
+
+# =========
+if a < b :
+    a, b = b, a
+
+while b!=0:
+    a, b = b, a % b
+
+print(a)
