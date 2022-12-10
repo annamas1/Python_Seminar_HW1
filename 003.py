27241cc52783fb52d82b37c21776b96316dc9f5d
anna maslova <zorina.a.1@yandex.ru>
2022-12-10 15:08:28 +0300

    002

--- 003.py
+++ 003.py
@@ -1,23 +1,23 @@
 #1. Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N
 
 # 1 вариант решения:
-# N = int(input())
-# M = N*(-1)
+N = int(input())
+M = N*(-1)
 # print(M)
 
-# while N != M:
-#     print(N)
-#     N -= 1
-# else:
-#     print(N)
-#     print('это все')
+while N != M:
+    print(N)
+    N -= 1
+else:
+    print(N)
+    print('это все')
 
 
 # 2 вариант решения:
 
-N = int(input("Введите размер списка: "))
-spam = list(range(-N, N+1))
-print(spam)
+# N = int(input("Введите размер списка: "))
+# spam = list(range(-N, N+1))
+# print(spam)
 
 
 # a = int(input())
